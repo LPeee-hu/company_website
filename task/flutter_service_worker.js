44cfdc4ec9ca680c689a8826d494df7035e456e2
LPeee-hu <LPeee-hu@users.noreply.github.com>
2025-04-12 22:19:29 +0000

    deploy: LPeee-hu/task_2@6dad2fb79eab314832833296b9d66b4f21399a71

--- task/flutter_service_worker.js
+++ task/flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"canvaskit/skwasm.js": "5d4f9263ec93efeb022bb14a3881d240",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "version.json": "244df336bb3136d0c0a78fef9dc5febd",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"main.dart.js": "f8352a73ac651ff2211f927fec667674",
+"main.dart.js": "92047dd1f015b3670c25fdadb4342f14",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "manifest.json": "320c136b9b22a309d242bf6828c8d7c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "008ecdd42be1a0a9773a951a534026f2"};
+"flutter_bootstrap.js": "f54e7bb0d36b4d8f9a589d486f6240a4"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",

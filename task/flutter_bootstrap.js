d40ebc364b60a61ae75ccba0b9213afd7cad8606
LPeee-hu <LPeee-hu@users.noreply.github.com>
2025-05-08 13:11:28 +0000

    deploy: LPeee-hu/task_2@f62814b4aa43db6e1bfdb7ed2fdac9b6f2e929bc

--- task/flutter_bootstrap.js
+++ task/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "902557695"
+    serviceWorkerVersion: "1929172125"
   }
 });
